6e6be3166b41212a135af1292495fa6723964647
virendra-jpg <sonkaria@iitk.ac.in>
2022-08-21 19:55:34 +0530

    Final Updates

--- src/comp/modal.js
+++ src/comp/modal.js
@@ -1,26 +1,29 @@
 
 import React from 'react'
 function closeModal() {
-    document.getElementById('myModal').remove()
+    document.getElementById('myModal').remove();
 }
-export default function Modal(){
+export default function Section0(){
     return(
         <>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
-<div id="myModal" style={{opacity:5,position:'absolute',top:250,zIndex:100, margin: 'auto'}}>
+<div id="myModal" style={{opacity:1}}>
 
-            <div className="modal-header">
+            {/* <div className="modal-header"> */}
+            {/* <div style={{ display: 'flex', alignItems: 'center'}}> */}
+            <button type="button" className="close" data-dismiss="modal" onClick={closeModal} >&times;</button>
             <a href="https://study.purpletutor.com/enroll.html?source=IITK" target="_blank" >
-            <img src='./topbanner.jpeg' style={{width:'100%'}} />
+            <img src='./topbanner.jpeg' style={{height:"10%", width:"100%", objectFit:'fill'}} ></img>
 
             </a>
-                <button style={{fontSize: '2rem', color: 'green' , padding: '0'}} type="button" className="close" data-dismiss="modal" onClick={closeModal}>&times;</button>
+            
+            {/* </div> */}
 
     </div>
-</div>
+{/* </div> */}
 </>
 );
 }
