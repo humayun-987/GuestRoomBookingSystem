6e061b9217abcc8375c830a9772ecb34e7ce34ab
Deepanshu yadav <depanshuyadavdp2020@gmail.com>
2023-07-16 22:57:01 +0530

    arranged

--- src/comp/Navbar/index.jsx
+++ src/comp/Navbar/index.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
 
     <MDBNavbar id='navbar' expand='lg' light style={{ backgroundColor: '#e3f2fd', padding: '0' }}>
       <MDBContainer fluid>
-        <MDBNavbarBrand href='#'><img style={{width: '3rem'}} src='./logo.png' /></MDBNavbarBrand>
+        <MDBNavbarBrand href='/#'><img style={{width: '3rem'}} src='./logo.png' /></MDBNavbarBrand>
         <MDBNavbarToggler
           type='button'
           data-target='#navbarColor02'
@@ -39,18 +39,18 @@ export default function Navbar() {
         <MDBCollapse show={showNavColorThird} navbar>
           <MDBNavbarNav className='me-auto mb-2 mb-lg-0' style={{marginLeft:'15px' , marginRight:'15px'}}>
             <MDBNavbarItem className='active'>
-              <MDBNavbarLink style={{fontWeight: 'bold'}} aria-current='page' href='#about'>
+              <MDBNavbarLink style={{fontWeight: 'bold'}} aria-current='page' href='/#about'>
                 ABOUT US
               </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink style={{fontWeight: 'bold'}} href='#conduction'>CONDUCTION</MDBNavbarLink>
+              <MDBNavbarLink style={{fontWeight: 'bold'}} href='/#conduction'>CONDUCTION</MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink style={{fontWeight: 'bold'}} href='#perks'>PERKS</MDBNavbarLink>
+              <MDBNavbarLink style={{fontWeight: 'bold'}} href='/#perks'>PERKS</MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink style={{fontWeight: 'bold'}} href='#contact'>CONTACT US</MDBNavbarLink>
+              <MDBNavbarLink style={{fontWeight: 'bold'}} href='/#contact'>CONTACT US</MDBNavbarLink>
             </MDBNavbarItem>
             <button type="button" style={{marginLeft:'50%', backgroundColor: '#0d6efd' , padding: '0.75rem 0.375rem', borderRadius: '0.375rem',color: 'white', border: 'none', fontWeight: '400'}}> Register</button>
           </MDBNavbarNav>
