863fda3ce65318102e2cb2151876796b6c10d55b
Himan_S <myanshim17@gmail.com>
2023-07-18 17:39:57 +0530

    Footer updated, Himanshu.

--- src/comp/footer/index.jsx
+++ src/comp/footer/index.jsx
@@ -10,8 +10,8 @@ import 'mdb-ui-kit';
 const Footer = () => {
 return (
 	<div >
-
-<footer className="text-center text-white" >
+  <div className="spec bg-black">
+  <footer className="text-center text-white" >
  
  
   <div className="container pt-4">
@@ -19,7 +19,7 @@ return (
     <section className="mb-4">
     
       <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"style={{backgroundColor: '#f1f1f1'}}
+        className="btn btn-link btn-floating btn-lg text-white px-4 fs-2 i pt-0"
         href="https://www.facebook.com/udghosh.iitk/"
         role="button"
         data-mdb-ripple-color="dark"
@@ -27,7 +27,7 @@ return (
 
      
       <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
+        className="btn btn-link btn-floating btn-lg text-white px-4 fs-2 i pt-0"
         href="https://twitter.com/udghoshiitk?lang=en"
         role="button"
         data-mdb-ripple-color="dark"
@@ -36,20 +36,20 @@ return (
       
      
       <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
+        className="btn btn-link btn-floating btn-lg text-white px-4 fs-2 i pt-0"
         href="https://www.instagram.com/udghosh_iitk/"
         role="button"
         data-mdb-ripple-color="dark"
         ><i className="bi bi-instagram"></i></a>
 
       <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
+        className="btn btn-link btn-floating btn-lg text-white px-4 fs-2 i pt-0"
         href="https://www.linkedin.com/company/udghosh-iit-kanpur-fest/mycompany/"
         role="button"
         data-mdb-ripple-color="dark"
         ><i className="bi bi-linkedin"></i></a>
       <a
-        className="btn btn-link btn-floating btn-lg text-dark m-1"
+        className="btn btn-link btn-floating btn-lg text-white px-4 fs-2 i pt-0"
         href="https://www.youtube.com/user/udghosh11"
         role="button"
         data-mdb-ripple-color="dark"
@@ -57,15 +57,18 @@ return (
     </section>
 
   </div>
-  <div className="text-center text-dark p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-    © 2022 Copyright:  
-    <a className="text-dark" target='blank' href= "https://udghosh.org.in/">  UDGHOSH22</a>
+
+  <div className="pb-5 pt-2">
+    <a href="#top"><i class="bi bi-chevron-double-up font-adjust"></i></a>
   </div>
-  <div class="last">
-      rahul;
-   </div>
+
+  <div className="text-center text-white p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+    © 2022 Copyright:  
+    <a className="text-white" target='blank' href= "https://udghosh.org.in/">  UDGHOSH22</a>
+  </div>
 </footer>
     </div>
+  </div>
     
 );
 
