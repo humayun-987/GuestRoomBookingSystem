e45dd5611daf7134c4deba72d2d59164e89a5862
Jenil-Dobariya <dobariyajb22@iitk.ac.in>
2023-07-30 14:19:08 +0530

    Modified highlight section

--- src/comp/Highlights/index.jsx
+++ src/comp/Highlights/index.jsx
@@ -1,137 +1,140 @@
 import "./style.css";
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronUp,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Highlights = () => {
-  const [isPrevEnabled, setIsPrevEnabled] = useState(false);
-  const [isNextEnabled, setIsNextEnabled] = useState(true);
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % photo.length);
+    }, 5000); 
+
+    return () => clearInterval(interval);
+  }, []);
+
+  
+
+  const icon1 = windowWidth < 500 ? faChevronLeft : faChevronUp;
+  const icon2 = windowWidth < 500 ? faChevronRight : faChevronDown;
 
   const photo = [
     {
-        "head":"1st Image",
-        "text":"I am just some dummy text for the dummy image shown in a dummy page",
-        "id": "53063766478",
-        "secret": "d98c207823",
-        "server": "65535",
-        "farm": 66,
-        "title": "Day 03, Udghosh'22 (66)",
-        "isprimary": "0",
-        "ispublic": 1,
-        "isfriend": 0,
-        "isfamily": 0
+      head: "Exciting Prizes",
+      text: "Medals & gadgets worth ₹50k+ for top 3 winners - Over 4000 participants competed last year!",
+      id: "53079516743",
+      secret: "48c58757e0",
+      server: "65535",
+      farm: 66,
+      title: "unosq-01 (1)",
+      isprimary: "0",
+      ispublic: 1,
+      isfriend: 0,
+      isfamily: 0,
     },
     {
-        "head":"2nd Image",
-        "text":"I am just some dummy text for the dummy image shown in a dummy page",
-        "id": "53063459699",
-        "secret": "f6876c71af",
-        "server": "65535",
-        "farm": 66,
-        "title": "Day 03, Udghosh'22 (69)",
-        "isprimary": "0",
-        "ispublic": 1,
-        "isfriend": 0,
-        "isfamily": 0
+      head: "Guest Lectures",
+      text: "Inspiring talks by distinguished teachers - Last year, we had veteran Teacher M.S. Chauhan sir!",
+      id: "53063770743",
+      secret: "bd7ca6566a",
+      server: "65535",
+      farm: 66,
+      title: "8B0066D9-E13F-41A1-BF5E-D06149DF41E4_",
+      isprimary: "0",
+      ispublic: 1,
+      isfriend: 0,
+      isfamily: 0,
     },
     {
-        "head":"3rd Image",
-        "text":"I am just some dummy text for the dummy image shown in a dummy page",
-        "id": "53063664270",
-        "secret": "3d99261925",
-        "server": "65535",
-        "farm": 66,
-        "title": "Day 03, Udghosh'22 (64)",
-        "isprimary": "0",
-        "ispublic": 1,
-        "isfriend": 0,
-        "isfamily": 0
-    },
-    {
-        "head":"4th Image",
-        "text":"I am just some dummy text for the dummy image shown in a dummy page",
-        "id": "53063274976",
-        "secret": "aeb2a47a3f",
-        "server": "65535",
-        "farm": 66,
-        "title": "Day 03, Udghosh'22 (65)",
-        "isprimary": "0",
-        "ispublic": 1,
-        "isfriend": 0,
-        "isfamily": 0
-    },
-    {
-        "head":"5th Image",
-        "text":"I am just some dummy text for the dummy image shown in a dummy page",
-        "id": "53063770743",
-        "secret": "bd7ca6566a",
-        "server": "65535",
-        "farm": 66,
-        "title": "8B0066D9-E13F-41A1-BF5E-D06149DF41E4_",
-        "isprimary": "0",
-        "ispublic": 1,
-        "isfriend": 0,
-        "isfamily": 0
-    },
-    {
-        "head":"6th Image",
-        "text":"I am just some dummy text for the dummy image shown in a dummy page",
-        "id": "53063789548",
-        "secret": "5b1f575a91",
-        "server": "65535",
-        "farm": 66,
-        "title": "istockphoto-1245120028-612x612",
-        "isprimary": "0",
-        "ispublic": 1,
-        "isfriend": 0,
-        "isfamily": 0
+      head: "Felicitation Ceremony",
+      text: "Honor and fame at IIT Kanpur - Olympic Silver Medalist Sakhshi Malik mam graced us last year!",
+      id: "53063459699",
+      secret: "f6876c71af",
+      server: "65535",
+      farm: 66,
+      title: "Day 03, Udghosh'22 (69)",
+      isprimary: "1",
+      ispublic: 1,
+      isfriend: 0,
+      isfamily: 0,
     }
-];
+  ];
 
-  // Click event handler for the previous button
   const handlePrevClick = () => {
-    const newIndex = currentIndex - 1;
+    let newIndex = currentIndex - 1;
+    if(newIndex<0) newIndex+=3;
+
+    newIndex=newIndex%3;
     setCurrentIndex(newIndex);
-    setIsPrevEnabled(newIndex > 0);
-    setIsNextEnabled(true);
   };
 
-  // Click event handler for the next button
   const handleNextClick = () => {
-    const newIndex = currentIndex + 1;
+    const newIndex = (currentIndex + 1)%3;
     setCurrentIndex(newIndex);
-    setIsNextEnabled(newIndex < photo.length - 1);
-    setIsPrevEnabled(true);
   };
 
   return (
+    <>
+    <h1 className="d-flex justify-content-center header">Perks and Rewards</h1>
     <div id="highlights">
       <div id="highlights-text-bg"></div>
       <div id="highlights-container">
         <div id="highlights-text">
           <div className="highlights-text-arrowBtns">
-            <button id="highlights-prevBtn" disabled={!isPrevEnabled} onClick={handlePrevClick}>
-              <FontAwesomeIcon icon={faChevronUp} />
+            <button
+              id="highlights-prevBtn"
+              onClick={handlePrevClick}
+            >
+              <FontAwesomeIcon icon={icon1} />
             </button>
           </div>
-          <div id="highlights-text-title">
-            <h3>{photo[currentIndex].head}</h3>
-          </div>
-          <div id="highlights-text-text">
-            <p>{photo[currentIndex].text}</p>
+          <div id="highlights-text-title-parent">
+            <div id="highlights-text-title">
+              <h3>{photo[currentIndex].head}</h3>
+            </div>
+            <div id="highlights-text-text">
+              <p>{photo[currentIndex].text}</p>
+            </div>
           </div>
           <div className="highlights-text-arrowBtns">
-            <button id="highlights-nextBtn" disabled={!isNextEnabled} onClick={handleNextClick}>
-              <FontAwesomeIcon icon={faChevronDown} />
+            <button
+              id="highlights-nextBtn"
+              onClick={handleNextClick}
+            >
+              <FontAwesomeIcon icon={icon2} />
             </button>
           </div>
         </div>
         <div id="highlights-img">
-          <img src={`https://live.staticflickr.com/${photo[currentIndex].server}/${photo[currentIndex].id}_${photo[currentIndex].secret}.jpg`} alt={photo[currentIndex].head} />
+          <img
+            src={`https://live.staticflickr.com/${photo[currentIndex].server}/${photo[currentIndex].id}_${photo[currentIndex].secret}.jpg`}
+            alt={photo[currentIndex].head}
+          />
         </div>
       </div>
     </div>
+    </>
+    
   );
 };
 
