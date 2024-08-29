9debd1465cdb5b9f63a2567247a5b85638a692c1
Shourya Trikha <shouryat21@iitk.ac.in>
2024-08-29 22:49:03 +0530

    tech partner added

--- src/comp/header/index.jsx
+++ src/comp/header/index.jsx
@@ -1,5 +1,5 @@
 import "./style.css";
-import React, { useState } from "react"; // Import useState from React
+import React, { useState } from "react";
 import uns from "../../media/VECT2.png";
 import { NavLink, useNavigate } from "react-router-dom";
 import { Modal, Button } from "react-bootstrap";
@@ -7,121 +7,115 @@ import { Modal, Button } from "react-bootstrap";
 export default function Header() {
   const navigate = useNavigate();
   const [showRegistrationModal, setShowRegistrationModal] = useState(false);
-
-  // State for the second modal
   const [showPaymentModal, setShowPaymentModal] = useState(false);
 
-  // Handlers for the first modal
   const handleShowRegistrationModal = () => setShowRegistrationModal(true);
   const handleCloseRegistrationModal = () => setShowRegistrationModal(false);
-
-  // Handlers for the second modal
   const handleShowPaymentModal = () => setShowPaymentModal(true);
   const handleClosePaymentModal = () => setShowPaymentModal(false);
+
   return (
     <>
-      <section className="header-sec" style={{
-        height: "100vh",
-        width: "100%",
-        paddingTop: "60px",
-      }}>
-        <div className="lower-layer" style={{
-          position: "absolute",
-          zIndex: "-2",
-          opacity: "0.15",
-          position: "fixed",
-        }}>
-          <img src="/bg-head.jpg" alt="" style={{
-            minHeight: "100vh",
+      <section
+        className="header-sec"
+        style={{
+          height: "100vh",
+          width: "100%",
+          paddingTop: "60px",
+        }}
+      >
+        <div
+          className="lower-layer"
+          style={{
+            position: "absolute",
+            zIndex: "-2",
+            opacity: "0.15",
+            position: "fixed",
+          }}
+        >
+          <img
+            src="/bg-head.jpg"
+            alt=""
+            style={{
+              minHeight: "100vh",
+              width: "100%",
+            }}
+          />
+        </div>
+        <div
+          className="lower-layer"
+          style={{
+            position: "absolute",
             width: "100%",
-          }} />
-        </div>
-        <div className="lower-layer" style={{
-          position: "absolute",
-          width: "100%",
-          zIndex: "-1",
-          height: "100vh",
-          opacity: "0.4",
-          top: "0",
-          // backgroundColor: var(--bg-header-lower-layer-back),
-        }}>
-        </div>
-        <div className="upper-layer" style={{
-          position: "absolute",
-          width: "100%",
-          zIndex: "5",
-          height: "100vh",
-          opacity: "0.4",
-          top: "0",
-          pointerEvents: "none",
-        }}>
-        </div>
+            zIndex: "-1",
+            height: "100vh",
+            opacity: "0.4",
+            top: "0",
+          }}
+        ></div>
+        <div
+          className="upper-layer"
+          style={{
+            position: "absolute",
+            width: "100%",
+            zIndex: "5",
+            height: "100vh",
+            opacity: "0.4",
+            top: "0",
+            pointerEvents: "none",
+          }}
+        ></div>
 
         <div className="header-div">
           <div className="unosq-icons">
             <div className="unosq-img">
               <img
-                style={{ 
-                  margin: "auto", 
-                  display: "block", 
-                  width: "350px", 
-                  height: "350px", 
-                  transform: "translateY(20px)" 
+                style={{
+                  margin: "auto",
+                  display: "block",
+                  width: "350px",
+                  height: "350px",
+                  transform: "translateY(20px)",
                 }}
                 src="./UNOSQ_new_logo.png"
               />
-                          {/* <div className="spon">
+            </div>
+
+            {/* Moved Sponsor Section */}
+            <div className="spon" style={{ marginTop: "60px", marginRight: "50px" }}>
               <div className="spon-item">
                 <div className="spon-title">
-                  <p>
-                    Presented By
-                  </p>
+                  <h5>Technology Partner</h5>
                 </div>
                 <div className="spon-img">
-                  <a href="https://www.pw.live/" target="_blank" id="hyperlinking">
-
-                    <img
-                      className="pw"
-                      src="/pw-1.png"
-                    />
+                  <a href="https://hiremee.co.in/" target="_blank" id="hyperlinking">
+                    <img className="pw" src="/hiremee.jpg" />
                   </a>
                 </div>
-
               </div>
-              <div className="spon-item">
+              {/* <div className="spon-item">
                 <div className="spon-title">
-                  <p>
-                    Powered By
-                  </p>
+                  <p>Powered By</p>
                 </div>
                 <div className="spon-img">
                   <a href="https://www.extramarks.com/students/k-12?utm_source=iitkanpur&utm_medium=unosq&utm_campaign=website" target="_blank" id="hyperlinking">
-                    <img
-                      className="em"
-                      src="/Extramarks.png"
-                    />
+                    <img className="em" src="/Extramarks.png" />
                   </a>
                 </div>
               </div>
               <div className="spon-item">
                 <div className="spon-title">
-                  <p>
-                    In Association With
-                  </p>
+                  <p>In Association With</p>
                 </div>
                 <div className="spon-img">
                   <a href="https://www.speedexam.net/" target="_blank" id="hyperlinking">
-                    <img
-                      className="se"
-                      src="/speedexam_logo.png"
-                    />
+                    <img className="se" src="/speedexam_logo.png" />
                   </a>
                 </div>
-              </div>
-            </div> */}
+              </div> */}
             </div>
-
           </div>
+
           <div className="picAndReg-btn">
             <div className="unosq-img">
               <img
@@ -132,22 +126,38 @@ export default function Header() {
                 src={uns}
               />
             </div>
-             {/* First Button and Modal */}
+
+            {/* First Button and Modal */}
             <div className="reg-in-header">
-              <Button variant="primary" className="bs-a-n register-button" onClick={handleShowRegistrationModal}>
+              <Button
+                variant="primary"
+                className="bs-a-n register-button"
+                onClick={handleShowRegistrationModal}
+              >
                 Register Now
               </Button>
 
-              <Modal show={showRegistrationModal} onHide={handleCloseRegistrationModal}>
+              <Modal
+                show={showRegistrationModal}
+                onHide={handleCloseRegistrationModal}
+              >
                 <Modal.Header closeButton>
                   <Modal.Title>Choose Registration Type</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                   <div className="d-flex justify-content-around">
-                    <NavLink to="/individualregister" className="btn btn-primary" onClick={handleCloseRegistrationModal}>
+                    <NavLink
+                      to="/individualregister"
+                      className="btn btn-primary"
+                      onClick={handleCloseRegistrationModal}
+                    >
                       Individual Registration
                     </NavLink>
-                    <NavLink to="/contingentregister" className="btn btn-secondary" onClick={handleCloseRegistrationModal}>
+                    <NavLink
+                      to="/contingentregister"
+                      className="btn btn-secondary"
+                      onClick={handleCloseRegistrationModal}
+                    >
                       Contingent Registration
                     </NavLink>
                   </div>
@@ -155,43 +165,44 @@ export default function Header() {
               </Modal>
             </div>
 
-            <br/>
+            <br />
 
             {/* Second Button and Modal */}
             <div className="reg-in-header">
-              <Button variant="primary" className="bs-a-n register-button" onClick={handleShowPaymentModal}>
+              <Button
+                variant="primary"
+                className="bs-a-n register-button"
+                onClick={handleShowPaymentModal}
+              >
                 Pay Now
               </Button>
 
-              <Modal show={showPaymentModal} onHide={handleClosePaymentModal}>
+              <Modal
+                show={showPaymentModal}
+                onHide={handleClosePaymentModal}
+              >
                 <Modal.Header closeButton>
                   <Modal.Title>Choose Payment Type</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                   <div className="d-flex justify-content-around">
-                    <NavLink to="/individualpayment" className="btn btn-primary" onClick={handleClosePaymentModal}>
+                    <NavLink
+                      to="/individualpayment"
+                      className="btn btn-primary"
+                      onClick={handleClosePaymentModal}
+                    >
                       Individual Payment
                     </NavLink>
-                    <NavLink to="/contingentpayment" className="btn btn-secondary" onClick={handleClosePaymentModal}>
+                    <NavLink
+                      to="/contingentpayment"
+                      className="btn btn-secondary"
+                      onClick={handleClosePaymentModal}
+                    >
                       Contingent Payment
                     </NavLink>
                   </div>
                 </Modal.Body>
               </Modal>
-            </div>
-
-
-
-
-            <div className="reg-in-header">
-              {/* <NavLink className='bs-a-n register-button' to="/individualregister" smooth duration={500}>
-                <p className="nav-item-text">
-                  Register Now
-                </p>
-              </NavLink> */}
-              {/* <Button variant="primary" className="bs-a-n register-button" onClick={() => navigate("/payment")}>
-                Pay Now
-              </Button> */}
             </div>
           </div>
         </div>
@@ -199,77 +210,3 @@ export default function Header() {
     </>
   );
 }
-
-
-/* <div id="sponsor_front" style={{
-  margin: "auto",
-  paddingTop: "60px",
-}} className="container">
-  <div className="row">
-    <div className="col-lg-6 col-md-6 col-sm-12 center">
-      <div className="row">
-        <img
-          className="col-lg-12 col-md-12 col-sm-12"
-          style={{
-            width: "50%",
-            margin: "auto",
-            alignItems: "center",
-            display: "block",
-          }}
-          src="./PurpleTutor.png"
-        />
-        <h2 style={{ marginBottom: "5%", textAlign: "center" }}>
-          Presents
-        </h2>
-        <img
-          className="col-lg-12 col-md-12 col-sm-12"
-          style={{ width: "100%", margin: "auto", display: "block" }}
-          src="./UNOSQ-01.png"
-        />
-      </div>
-      <div className="row">
-        <div className="col-lg-6 col-md-6 col-sm-6">
-          <h3>In Association With</h3>
-          <img style={{ width: '100%' }} src="http://cdn.shopify.com/s/files/1/0260/9384/6583/files/Scooboo.png?v=1650348175" />
-        </div>
-        <div className="col-lg-6 col-md-6 col-sm-6">
-          <h3>Powered By</h3>
-          <img style={{ width: '100%' }} src="https://s3.ap-south-1.amazonaws.com/assets.ynos.in/startup-logos/YNOS315064.png" />
-        </div>
-
-      </div>
-    </div>
-
-    <div className="col-lg-6 col-md-6 col-sm-12">
-      <img
-        style={{
-          width: "100%",
-          alignItems: "center",
-          marginLeft: "auto",
-          marginRight: "auto",
-          marginTop: "30px",
-        }}
-        src={uns}
-      />
-      <NavLink to="/register">
-        <button
-          style={{
-            margin: "auto",
-            backgroundColor: "rgb(13, 10, 253)",
-            padding: "0.75rem 0.375rem",
-            display: "flex",
-            borderRadius: "0.375rem",
-            color: "white",
-            border: "none",
-            fontWeight: "400",
-          }}
-        >
-          {" "}
-          Register Now
-        </button>{" "}
-      </NavLink>
-
-    </div>
-  </div>
-
-</div> */
