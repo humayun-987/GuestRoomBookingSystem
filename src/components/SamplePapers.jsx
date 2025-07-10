2f62a1a63ac0fad9166873d385ef79c47100911e
Humayun <humayunahmad987@gmail.com>
2025-07-10 21:36:36 +0530

    sample papers added

--- src/components/SamplePapers.jsx
+++ src/components/SamplePapers.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./SamplePapers.css"; // Add styles separately
 import SizedBox from "./SizedBox";
 const samplePapers = [
-  { title: "Pool Little Champs", link: "#" },
-  { title: "Pool Super Nova", link: "#" },
-  { title: "Pool The Titans", link: "#" },
-  { title: "Pool Elite Explorers", link: "#" },
+  { title: "Pool Little Champs", link: "https://drive.usercontent.google.com/u/0/uc?id=1pzmCU9R1RwA9QTe0wVR3GWL7urIsKSbi&export=download" },
+  { title: "Pool Super Nova", link: "https://drive.usercontent.google.com/u/0/uc?id=1Us9O-aYHY8Vy3PUi8tEI95KqmS8LS5th&export=download" },
+  { title: "Pool The Titans", link: "https://drive.usercontent.google.com/u/0/uc?id=1RoubVWjnbKc_evdgfMYJepY8bGE1tZvZ&export=download" },
+  { title: "Pool Elite Explorers", link: "https://drive.usercontent.google.com/u/0/uc?id=1Cz3PwK3tg4e9F6u7r17GWFe6XK7OP4au&export=download" },
 ];
 
 const SamplePapers = () => {
